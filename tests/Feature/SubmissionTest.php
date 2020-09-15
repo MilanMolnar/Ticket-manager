12a4e2805c027197b9de29bad58d3d747f5f6eaf
Milan Molnar <molnarmilan.cc@gmail.com>
2020-09-15 11:25:37 +0200

    due date calculation is done, finishing touches comments and validation

---
+++ tests/Feature/SubmissionTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class SubmissionTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+
+    
+
+}
